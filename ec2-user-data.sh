190522bd16c71796f589c5c6def5c536294d24ef
tighiltfd <tighiltfd@gmail.com>
2025-02-08 20:25:00 -0500

    Update ec2-user-data.sh

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -5,4 +5,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Hello from PIIT from $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<h1>Hello from PIIT from (Server1) $(hostname -f)</h1>" > /var/www/html/index.html
